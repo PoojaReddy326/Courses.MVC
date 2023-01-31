b76ba72ac5d5acf96dd0c42b2aaf1d13491d4bd9
Poojareddy Gaddam <poojareddyg@infinite.com>
2023-01-31 16:55:31 +0530

    Update  Amin MVC

---
+++ Courses.MVC/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using Courses.MVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Courses.MVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public AdminController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(AdminViewModel admin)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    client.BaseAddress = new System.Uri(_configuration["ApiUrl:api"]);
+                    var result = await client.PostAsJsonAsync("Admin/Login", admin);
+                    if (result.IsSuccessStatusCode)
+                    {
+                        string token = await result.Content.ReadAsAsync<string>();
+                        HttpContext.Session.SetString("token", token);
+                        return RedirectToAction("Privacy", "Home");
+
+                    }
+                    ModelState.AddModelError("", "Invalid Username or Password");
+                }
+            }
+            return View(admin);
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("token");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Courses.MVC/Models/AdminViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Courses.MVC.Models
+{
+    public class AdminViewModel:Login
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //[Required]
+        //public string Email { get; set; }
+        //[Required]
+        //public string Password { get; set; }
+    }
+    public class Login
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
+
